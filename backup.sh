1eca1b967cba3e4f447bd52c734ea1870db14245
Sainath107 <sainathsaip07@gmail.com>
2026-09-16 18:19:08 +0530

    added change to backup "Integrate AWS S3 sync for backup completion"
    Add AWS S3 sync command to backup script

--- backup.sh
+++ backup.sh
@@ -12,5 +12,9 @@ dest=$2
 timestamp=$(date '+%Y-%m-%d-%H-%M')
 
 zip -r "$dest/backup-$timestamp.zip" $src > /dev/null
+<<cmd 
+	aws s3 sync $dest s3://tws-backups-d
+	backup completed and added to s3
+cmd 
 
 echo "Backup Done"
